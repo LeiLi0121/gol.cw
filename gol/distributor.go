bd5caebcac8f7700c36ed13821952c2a70dee168
LeiLi0121 <en22655@bristol.ac.uk>
2023-11-01 21:06:19 +0000

    step1

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,10 @@
 package gol
 
+import (
+	"fmt"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -11,23 +16,125 @@ type distributorChannels struct {
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
-
 	// TODO: Create a 2D slice to store the world.
+	fileName := fmt.Sprintf("%dx%d", p.ImageHeight, p.ImageWidth)
+	//fmt.Println(fileName)
 
-	turn := 0
+	c.ioCommand <- ioCommand(1)
+	c.ioFilename <- fileName
+	fmt.Println("filename sended")
+	//world recieve the image from io
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.ImageWidth)
+	}
+	for i := 0; i < p.ImageHeight; i++ {
+		for k := 0; k < p.ImageWidth; k++ {
+			world[i][k] = <-c.ioInput
+		}
+	}
 
+	//new world is to store next state
+	newWorld := make([][]byte, p.ImageHeight)
+	for i := range newWorld {
+		newWorld[i] = make([]byte, p.ImageWidth)
+	}
+	//set newworld same as world at first
+	newWorld = world
 
+	turn := p.Turns
 	// TODO: Execute all turns of the Game of Life.
-
+	for t := 0; t < turn; t++ {
+		newWorld = calculateNextState(world)
+		world = newWorld
+	}
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
-
+	alive := calculateAliveCells(newWorld)
+	c.events <- FinalTurnComplete{CompletedTurns: turn, Alive: alive}
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
 	c.events <- StateChange{turn, Quitting}
-	
+
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
+}
+
+func calculateNearAlive(world [][]byte, row, col int) int {
+	adjacent := []Pair{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+	//add to get all adj nodes within 0-15
+	for i := 0; i < 8; i++ {
+		adjacent[i].y += row
+		if adjacent[i].y == len(world) {
+			adjacent[i].y = 0
+		}
+		if adjacent[i].y == -1 {
+			adjacent[i].y = len(world) - 1
+		}
+		adjacent[i].x += col
+		if adjacent[i].x == len(world[i]) {
+			adjacent[i].x = 0
+		}
+		if adjacent[i].x == -1 {
+			adjacent[i].x = len(world[i]) - 1
+		}
+	}
+	//count alive using for
+	count := 0
+	for _, node := range adjacent {
+		if world[node.y][node.x] == 255 {
+			count++
+		}
+	}
+	return count
+}
+
+func calculateNextState(world [][]byte) [][]byte {
+
+	newWorld := make([][]byte, len(world))
+	for i := range newWorld {
+		newWorld[i] = make([]byte, len(world[i]))
+	}
+	for i := 0; i < len(world); i++ { //each row
+		for k := 0; k < len(world[i]); k++ { //each col
+			numOfAlive := calculateNearAlive(world, i, k)
+			currentNode := world[i][k]
+
+			//rules for updating the cell state
+			if world[i][k] == 255 {
+				if numOfAlive < 2 {
+					newWorld[i][k] = 0
+				} else if numOfAlive == 2 || numOfAlive == 3 {
+					newWorld[i][k] = currentNode
+				} else if numOfAlive > 3 {
+					newWorld[i][k] = 0
+				}
+			} else if currentNode == 0 && numOfAlive == 3 {
+				newWorld[i][k] = 255
+			}
+		}
+	}
+
+	return newWorld
+}
+func calculateAliveCells(world [][]byte) []util.Cell {
+	var alive []util.Cell
+	for i := 0; i < len(world); i++ {
+		for k := 0; k < len(world[i]); k++ {
+			if world[i][k] == 255 {
+				alive = append(alive, util.Cell{X: k, Y: i})
+			}
+		}
+	}
+	return alive
+}
+
+type Pair struct {
+	y int
+	x int
 }
