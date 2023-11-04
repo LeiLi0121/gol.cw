e702d003b30caf3719017ead05eee71f187246bf
LeiLi0121 <en22655@bristol.ac.uk>
2023-11-04 17:35:51 +0000

    step2 and step3(not yet complete)

--- gol/distributor.go
+++ gol/distributor.go
@@ -2,6 +2,7 @@ package gol
 
 import (
 	"fmt"
+	"time"
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
@@ -18,12 +19,9 @@ type distributorChannels struct {
 func distributor(p Params, c distributorChannels) {
 	// TODO: Create a 2D slice to store the world.
 	fileName := fmt.Sprintf("%dx%d", p.ImageHeight, p.ImageWidth)
-	//fmt.Println(fileName)
-
 	c.ioCommand <- ioCommand(1)
 	c.ioFilename <- fileName
-	fmt.Println("filename sended")
-	//world recieve the image from io
+	//world recieve the image from io-----------------------------------------------------------------------------------
 	world := make([][]byte, p.ImageHeight)
 	for i := range world {
 		world[i] = make([]byte, p.ImageWidth)
@@ -33,23 +31,77 @@ func distributor(p Params, c distributorChannels) {
 			world[i][k] = <-c.ioInput
 		}
 	}
-
-	//new world is to store next state
-	newWorld := make([][]byte, p.ImageHeight)
-	for i := range newWorld {
-		newWorld[i] = make([]byte, p.ImageWidth)
-	}
-	//set newworld same as world at first
-	newWorld = world
+	//new world is to store next state----------------------------------------------------------------------------------
 
 	turn := p.Turns
-	// TODO: Execute all turns of the Game of Life.
-	for t := 0; t < turn; t++ {
-		newWorld = calculateNextState(world)
-		world = newWorld
+	if turn != 0 {
+		completedTurn := 0
+		ticker := time.NewTicker(2 * time.Second)
+		go func() {
+			for {
+				select {
+				case <-ticker.C:
+					c.events <- AliveCellsCount{CompletedTurns: completedTurn, CellsCount: len(calculateAliveCells(world))}
+				}
+			}
+		}()
+		newWorld := make([][]byte, p.ImageHeight)
+		for i := range newWorld {
+			newWorld[i] = make([]byte, p.ImageWidth)
+		}
+		copyWhole(newWorld, world)
+		if p.Threads == 1 {
+			// Single-threaded execution
+			for t := 0; t < turn; t++ {
+				newWorld = calculateNextState(0, p.ImageHeight, world)
+				copyWhole(world, newWorld)
+				completedTurn++
+			}
+		} else {
+			portionHeight := p.ImageHeight / p.Threads
+			for t := 0; t < turn; t++ {
+				parts := make([]chan [][]uint8, p.Threads)
+				for i := range parts {
+					parts[i] = make(chan [][]uint8, 1)
+				}
+				for i := 0; i < p.Threads; i++ {
+					go func(i int) {
+						if i == p.Threads-1 {
+							startY := i * portionHeight
+							endY := p.ImageHeight
+							parts[i] <- calculateNextState(startY, endY, world)
+						} else {
+							startY := i * portionHeight
+							endY := (i + 1) * portionHeight
+							parts[i] <- calculateNextState(startY, endY, world)
+						}
+					}(i)
+
+				}
+				for i := 0; i < p.Threads; i++ {
+					part := <-parts[i]
+					if i != p.Threads-1 {
+						for h := 0; h < portionHeight; h++ {
+							copy(newWorld[i*portionHeight+h], part[i*portionHeight+h])
+						}
+					} else {
+						for h := 0; h < p.ImageHeight-i*portionHeight; h++ {
+							copy(newWorld[i*portionHeight+h], part[i*portionHeight+h])
+						}
+					}
+
+				}
+				copyWhole(world, newWorld)
+				completedTurn++
+				c.events <- TurnComplete{CompletedTurns: t}
+			}
+
+		}
+		defer ticker.Stop()
 	}
+
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-	alive := calculateAliveCells(newWorld)
+	alive := calculateAliveCells(world)
 	c.events <- FinalTurnComplete{CompletedTurns: turn, Alive: alive}
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
@@ -94,14 +146,19 @@ func calculateNearAlive(world [][]byte, row, col int) int {
 	return count
 }
 
-func calculateNextState(world [][]byte) [][]byte {
+func copyWhole(dst, src [][]byte) {
+	for i := range src {
+		copy(dst[i], src[i])
+	}
+}
+func calculateNextState(startY, endY int, world [][]byte) [][]byte {
 
 	newWorld := make([][]byte, len(world))
 	for i := range newWorld {
 		newWorld[i] = make([]byte, len(world[i]))
 	}
-	for i := 0; i < len(world); i++ { //each row
-		for k := 0; k < len(world[i]); k++ { //each col
+	for i := startY; i < endY; i++ { //each row
+		for k := 0; k < len(world[i]); k++ { //each item in row
 			numOfAlive := calculateNearAlive(world, i, k)
 			currentNode := world[i][k]
 
