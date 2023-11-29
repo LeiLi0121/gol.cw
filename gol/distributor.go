d6e10331273539020735d25c1bf52a00b7ee3af5
LeiLi0121 <128519250+LeiLi0121@users.noreply.github.com>
2023-11-29 18:34:02 +0000

    final edition

--- gol/distributor.go
+++ gol/distributor.go
@@ -22,15 +22,13 @@ func distributor(p Params, c distributorChannels) {
 	fileName := fmt.Sprintf("%dx%d", p.ImageHeight, p.ImageWidth)
 	c.ioCommand <- ioCommand(1)
 	c.ioFilename <- fileName
-	//world recieve the image from io-----------------------------------------------------------------------------------
 	world := make([][]uint8, p.ImageHeight)
 	for i := range world {
 		world[i] = make([]uint8, p.ImageWidth)
 	}
-
 	for i := 0; i < p.ImageHeight; i++ {
 		for k := 0; k < p.ImageWidth; k++ {
-			world[i][k] = <-c.ioInput
+			world[i][k] = <-c.ioInput //world recieve the image from io
 		}
 	}
 	for i := 0; i < p.ImageHeight; i++ {
@@ -42,17 +40,13 @@ func distributor(p Params, c distributorChannels) {
 	}
 	turn := p.Turns
 	if turn != 0 {
-		//new world is to store next state----------------------------------------------------------------------------------
-		newWorld := make([][]uint8, p.ImageHeight)
+		newWorld := make([][]uint8, p.ImageHeight) //new world is to store next state
 		for i := range newWorld {
 			newWorld[i] = make([]uint8, p.ImageWidth)
 		}
-
-		ticker := time.NewTicker(2 * time.Second) //ticker send signal every 2 second to report alive cell
-
-		//var mutex sync.Mutex //a lock to protect livecell and completed without race condition
+		ticker := time.NewTicker(2 * time.Second)
 		liveCell := len(calculateAliveCells(world))
-		completed := 0
+		completed := 0 //used to store completed turn, since t is not exactly the turn completed
 		portionHeight := p.ImageHeight / p.Threads
 		copyWhole(newWorld, world)
 
@@ -65,18 +59,13 @@ func distributor(p Params, c distributorChannels) {
 					case 's':
 						// Save the game state
 						saveWorldState(c, p, world, fileName, completed)
-						fmt.Println("saved the current state")
 					case 'q':
 						// Save the game state and quit
 						saveWorldState(c, p, world, fileName, completed)
-						fmt.Println("saved the current state as final")
-						fmt.Println("final turn completed is", completed)
 						c.events <- FinalTurnComplete{CompletedTurns: completed, Alive: calculateAliveCells(world)}
-						// Make sure that the Io has finished any output before exiting.
 						c.ioCommand <- ioCheckIdle
 						<-c.ioIdle
 						c.events <- StateChange{turn, Quitting}
-						// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 						close(c.events)
 					case 'p':
 						// Pause the game
@@ -130,7 +119,7 @@ func distributor(p Params, c distributorChannels) {
 
 						}
 						for i := 0; i < p.Threads; i++ {
-							part := <-parts[i]
+							part := <-parts[i] //part ass sync tool
 							if i != p.Threads-1 {
 								for h := 0; h < portionHeight; h++ {
 									copy(newWorld[i*portionHeight+h], part[i*portionHeight+h])
@@ -138,7 +127,6 @@ func distributor(p Params, c distributorChannels) {
 							} else {
 								for h := 0; h < p.ImageHeight-i*portionHeight; h++ {
 									copy(newWorld[i*portionHeight+h], part[i*portionHeight+h])
-
 								}
 							}
 
@@ -262,19 +250,16 @@ func calculateAliveCells(world [][]uint8) []util.Cell {
 	return alive
 }
 
-// Helper function to save the current state of the world
 func saveWorldState(c distributorChannels, p Params, world [][]uint8, fileName string, completed int) {
-	c.ioCommand <- ioCommand(0)                               // Send a command to indicate the start of an I/O operation
-	c.ioFilename <- fmt.Sprintf("%s-%d", fileName, completed) // Send the filename to the I/O system
+	c.ioCommand <- ioCommand(0)
+	c.ioFilename <- fmt.Sprintf("%s-%d", fileName, completed)
 	for i := 0; i < p.ImageHeight; i++ {
 		for k := 0; k < p.ImageWidth; k++ {
-			c.ioOutput <- world[i][k] // Send each cell's state to the I/O system
+			c.ioOutput <- world[i][k]
 		}
 	}
 	c.events <- ImageOutputComplete{completed, fileName}
 }
-
-// Goroutine to handle key presses
 
 type Pair struct {
 	y int
